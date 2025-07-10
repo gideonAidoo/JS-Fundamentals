c49741cfc687e9131eefc4137e6aa496cf680931
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-07-10 21:11:44 +0000

    new line is added to 1-multi_languages.js file

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,5 +1,7 @@
-let lang = "C is fun";
-let python = "Python is cool";
-let javaScript = "javaScript is amazing";
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
 
-console.log(lang,python,javaScript);
+console.log(line1);
+console.log(line2);
+console.log(line3);
