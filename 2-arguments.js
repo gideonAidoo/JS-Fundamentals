ac5f19b0644e67c05aea3588a9eec338382027eb
Gideon Aidoo <gideonaidoo2016@gmail.com>
2025-07-10 20:52:52 +0000

    new argument file is added

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+
+const args = process.argv; // Gets arguments passed after the script name
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
